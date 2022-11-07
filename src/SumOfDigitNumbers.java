27d87a902a63aeae7d3973efcbe5404496437089
gcuysal <goktuguysal96@gmail.com>
2022-11-07 22:05:06 +0300

    PowerOfNumbers

---
+++ src/SumOfDigitNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfDigitNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = input.nextInt();
+        int tempNumber = number;
+        int digitsValue;
+        int result = 0;
+
+        while (tempNumber != 0) {
+            digitsValue = tempNumber % 10;
+            result += digitsValue;
+            tempNumber /= 10;
+        }
+        System.out.println("Sum of digits of the number " + number + ": " + result);
+    }
+}
